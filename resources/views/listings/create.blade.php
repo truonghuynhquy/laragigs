45b799a690fdac286d442a157fe05f167830a8c1
Trương Huỳnh Quý <19522110@gm.uit.edu.vn>
2023-08-10 17:10:20 +0700

    add data, flash-message

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -11,41 +11,81 @@
                 <label for="company" class="inline-block text-lg mb-2">Company Name</label>
                 <input type="text" class="border border-gray-200 rounded p-2 w-full" name="company"
                     value="{{ old('company') }}" />
+
+                @error('company')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                @enderror
             </div>
 
             <div class="mb-6">
                 <label for="title" class="inline-block text-lg mb-2">Job Title</label>
                 <input type="text" class="border border-gray-200 rounded p-2 w-full" name="title"
                     value="{{ old('company') }}" />
+
+                @error('title')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                @enderror
             </div>
 
             <div class="mb-6">
                 <label for="location" class="inline-block text-lg mb-2">Job Location</label>
                 <input type="text" class="border border-gray-200 rounded p-2 w-full" name="location"
                     value="{{ old('company') }}" />
+
+                @error('location')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                @enderror
+            </div>
+
+            <div class="mb-6">
+                <label for="email" class="inline-block text-lg mb-2">
+                    Contact Email
+                </label>
+                <input type="text" class="border border-gray-200 rounded p-2 w-full" name="email"
+                    value="{{ old('email') }}" />
+
+                @error('email')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                @enderror
             </div>
 
             <div class="mb-6">
                 <label for="website" class="inline-block text-lg mb-2">Website/Application URL</label>
                 <input type="text" class="border border-gray-200 rounded p-2 w-full" name="website"
                     value="{{ old('website') }}" />
+
+                @error('website')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                @enderror
             </div>
 
             <div class="mb-6">
                 <label for="tags" class="inline-block text-lg mb-2">Tags (Comma Separated)</label>
                 <input type="text" class="border border-gray-200 rounded p-2 w-full" name="tags"
                     placeholder="Example: Laravel, Backend, Postges, etc" value="{{ old('tags') }}" />
+
+                @error('tags')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                @enderror
             </div>
 
             <div class="mb-6">
                 <label for="logo" class="inline-block text-lg mb-2">Company Logo</label>
                 <input type="text" class="border border-gray-200 rounded p-2 w-full" name="logo" />
+
+                @error('logo')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                @enderror
             </div>
 
             <div class="mb-6">
                 <label for="description" class="inline-block text-lg mb-2">Job Description</label>
                 <textarea type="text" class="border border-gray-200 rounded p-2 w-full" name="description" row="10"
                     placeholder="Include tasks, requirements, salary, etc">{{ old('description') }}</textarea>
+
+                @error('description')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                @enderror
             </div>
 
             <div class="mb-6">
